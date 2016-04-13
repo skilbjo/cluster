752d4c7c9aa8e2f75f413642ec7d236aed7abe56
John Skilbeck <john.skilbeck@gmail.com>
2016-04-13 13:01:27 -0700

    python

---
+++ python_test/md5_attack.py
@@ -0,0 +1,15 @@
+import hashlib
+hash_to_crack = "098f6bcd4621d373cade4e832627b4f6"
+dict_file = "/home/pi/python_test/dict.txt"
+
+def main():
+    with open(dict_file) as fileobj:
+        for line in fileobj:
+            line = line.strip()
+            if hashlib.md5(line).hexdigest() == hash_to_crack:
+                print("successfully cracked the hash %s: It's %s") % (hash_to_crack, line)
+                return ""
+    print("FAiled to crack the file.")
+
+if __name__ == "__main__":
+    main()
